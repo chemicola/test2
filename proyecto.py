47dee2ed6f2def3ead8a7972bd7345c5a4607dc7
chemicola <jose741983@gmail.com>
2025-02-16 16:40:43 +0100

    Texto a voz: Funcional

--- proyecto.py
+++ proyecto.py
@@ -1,2 +1,18 @@
 
-print("Hola Clou")
+import pyttsx3 as tts
+
+# Inicializar el motor de texto a voz
+engine = tts.init()
+
+# Configuración de propiedades.
+engine.setProperty('rate', 500)  # <-- Velocidad de la voz
+engine.setProperty('volume', 0.5)  # <-- Volumen (0.0 a 1.0)
+
+# Texto que quieres convertir a voz.
+texto = "Este es un texto de ejemplo de mi primer proyecto, pasando de texto a voz."
+
+# Convertimos el texto a voz,
+engine.say(texto)
+
+# Reproducimos la voz.
+engine.runAndWait()
